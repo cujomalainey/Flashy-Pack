58c36acb3fc3dff2cbf08e19932ce09245e9bdd1
Curtis <curtis@malainey.com>
2015-03-15 14:52:27 -0600

    big change, don't remember what it was

--- CapacitiveSensorCode/CapacitiveSensorCode.ino
+++ CapacitiveSensorCode/CapacitiveSensorCode.ino
@@ -8,20 +8,11 @@ CapacitiveSensor   cs_9_2  = CapacitiveSensor(9,10);        // 10M resistor betw
 Adafruit_NeoPixel front = Adafruit_NeoPixel(24, 1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel back = Adafruit_NeoPixel(40, 12, NEO_GRB + NEO_KHZ800);
 
-// signal state trackers
-int leftSignal = 0;
-int rightSignal = 0;
-int lastLeftSignal = 0;
-int lastRightSignal = 0;
+uint8_t leftSignal = 0;
+uint8_t rightSignal = 0;
 
-// touch pad trackers
-long lastLeftState = 0;
-long lastRightState = 0;
-
-uint8_t leftSignalLED = 0; 
+uint8_t SignalLED = 0; 
 long leftSignalDelay = 0;
-
-uint8_t rightSignalLED = 0; 
 long rightSignalDelay = 0;
 
 uint8_t leftPadStatus = 0;
@@ -31,6 +22,17 @@ long padHoldTime = 0;
 uint8_t bothPadsHit = 0;
 uint8_t rightPadHit = 0;
 uint8_t leftPadHit = 0;
+
+void resetSignals(){
+  for (int i = 0; i < 16; i++){
+    back.setPixelColor(i, front.Color(0,0,0)); 
+  }
+  for (int i = 0; i < 24; i++){
+    front.setPixelColor(i, front.Color(255,255,255)); 
+  }
+  back.show();
+  front.show();
+}
 
 void checkPads() {
   long leftTotal  =  cs_9_10.capacitiveSensor(30);
@@ -45,8 +47,8 @@ void checkPads() {
       bothPadsHit = 0;
       leftPadHit = 0;
       rightPadHit = 0;
-      Serial.print("Both: ");
-      Serial.println(bothPadStatus);
+      // Serial.print("Both: ");
+      // Serial.println(bothPadStatus);
     }
     else {
       bothPadStatus = 0;
@@ -61,9 +63,12 @@ void checkPads() {
       if (leftTotal < 30) {
         leftPadStatus = ceil(((double)(millis() - padHoldTime))/1000.0);
         leftPadHit = 0;
-        Serial.print("Left: ");
-        Serial.println(leftPadStatus);
+        // Serial.print("Left: ");
+        // Serial.println(leftPadStatus);
       }
+    }
+    else {
+      leftPadStatus = 0;
     }
     if (rightTotal > 30 || rightPadHit) {
       if (!rightPadHit) {
@@ -73,119 +78,143 @@ void checkPads() {
       if (rightTotal < 30) {
         rightPadStatus = ceil(((double)(millis() - padHoldTime))/1000.0);
         rightPadHit = 0;
-        Serial.print("Right: ");
-        Serial.println(rightPadStatus);
+        // Serial.print("Right: ");
+        // Serial.println(rightPadStatus);
       }
     }
+    else {
+      rightPadStatus = 0;
+    }
+  }
+}
+
+void updateState()
+{
     // left signal control
-    // if (leftTotal > 30 && lastLeftState < 30){
-    //   if (leftSignal == 1){
-    //     for (int i = 0; i < 12; i++){
-    //       front.setPixelColor(i, front.Color(255,255,255)); 
-    //     }
-    //     for (int i = 0; i < 8; i++){
-    //       back.setPixelColor(i, front.Color(0,0,0)); 
-    //     }
-    //     back.show();
-    //     leftSignal = 0;
-    //     leftSignalLED = 0;
-    //   }
-    //   else {
-    //     leftSignal = 1;
-    //   }
-    //   front.show();
-    //   Serial.print("left pressed");
-    // }
-    // // right signal control
-    // if (total2 > 30 && lastRightState < 30){
-    //   if (rightSignal == 1){
-    //     for (int i = 12; i < 24; i++){
-    //       front.setPixelColor(i, front.Color(255,255,255)); 
-    //     }
-    //     rightSignal = 0;
-    //   }
-    //   else {
-    //     for (int i = 12; i < 24; i++){
-    //       front.setPixelColor(i, front.Color(255,90,0)); 
-    //     }  
-    //     rightSignal = 1;
-    //   }
-    //   front.show();
-    //   Serial.print("right pressed");
-    // }
-  }
+    if (leftPadStatus == 1){
+      if (leftSignal == 1){
+        resetSignals();
+        back.show();
+        leftSignal = 0;
+        SignalLED = 0;
+      }
+      else {
+        resetSignals();
+        leftSignal = 1;
+        rightSignal = 0;
+      }
+      front.show();
+    }
+    // right signal control
+    if (rightPadStatus == 1){
+      if (rightSignal == 1){
+        resetSignals();
+        rightSignal = 0;
+        SignalLED = 0;
+      }
+      else {
+        resetSignals();
+        rightSignal = 1;
+        leftSignal = 0;
+      }
+      front.show();
+    }
 }
 
-void setup()                    
-{
-  cs_9_10.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-  cs_9_2.set_CS_AutocaL_Millis(0xFFFFFFFF); 
-  Serial.begin(9600);
-  front.begin();
-  back.begin();
-  front.show();
-  for (int i = 16; i < 40; i++){
-    back.setPixelColor(i, front.Color(100,0,0)); 
-  }
-  back.show();
-}
-
-void loop()                    
-{
-  checkPads();
-  long start = millis();
-  
+void updateLedAnimations() {
   // left signal animations
   if (leftSignal && millis() - leftSignalDelay > 50 && !rightSignal) {
-    if (leftSignalLED == 0) {
+    if (SignalLED == 0) {
       for (int i = 0; i < 12; i++){
         front.setPixelColor(i, front.Color(255,90,0)); 
       }  
     }
-    if (leftSignalLED == 4) {
+    if (SignalLED == 4) {
       for (int i = 0; i < 12; i++){
         front.setPixelColor(i, front.Color(0,0,0)); 
       }  
     }
     front.show();
-    if (leftSignalLED == 8) {
-      leftSignalLED = 0;
+    if (SignalLED == 8) {
+      SignalLED = 0;
       for (uint8_t i = 0; i < 8; i++) {
         back.setPixelColor(i, back.Color(0,0,0));
       }
     }
     else {
-      back.setPixelColor(7 - leftSignalLED, back.Color(255,90,0));
-      leftSignalLED++;
+      back.setPixelColor(7 - SignalLED, back.Color(255,90,0));
+      SignalLED++;
     }
     back.show();
     leftSignalDelay = millis();
   }
   // right signal animations
   if (rightSignal && millis() - rightSignalDelay > 50 && !leftSignal) {
-    if (rightSignalLED == 0) {
+    if (SignalLED == 0) {
       for (int i = 12; i < 24; i++){
         front.setPixelColor(i, front.Color(255,90,0)); 
       }  
     }
-    if (rightSignalLED == 4) {
+    if (SignalLED == 4) {
       for (int i = 12; i < 24; i++){
         front.setPixelColor(i, front.Color(0,0,0)); 
       }  
     }
     front.show();
-    if (rightSignalLED == 8) {
-      rightSignalLED = 0;
+    if (SignalLED == 8) {
+      SignalLED = 0;
       for (uint8_t i = 8; i < 16; i++) {
         back.setPixelColor(i, back.Color(0,0,0));
       }
     }
     else {
-      back.setPixelColor(rightSignalLED + 8, back.Color(255,90,0));
-      rightSignalLED++;
+      back.setPixelColor(SignalLED + 8, back.Color(255,90,0));
+      SignalLED++;
     }
     back.show();
     rightSignalDelay = millis();
   }
+}
+
+void setup()                    
+{
+  //cs_9_10.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  //cs_9_2.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+  // Serial.begin(9600);
+  front.begin();
+  back.begin();
+  front.show();
+  uint8_t ready = 0;
+  for (int i = 0; i < 40; i++){
+    back.setPixelColor(i, front.Color(0,0,0)); 
+  }
+  for (int i = 0; i < 24; ++i)
+  {
+    front.setPixelColor(i, front.Color(0,0,0)); 
+  }
+  while(!ready) {
+    checkPads();
+    if (bothPadStatus >= 3) {
+      ready = 1;
+    }
+  }
+  for (int i = 16; i < 40; i++){
+    back.setPixelColor(i, front.Color(100,0,0)); 
+  }
+  back.show();
+  resetSignals();
+}
+int freeRam () 
+{
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+void loop()                    
+{
+  Serial.println(millis());
+  checkPads();
+  updateState();
+  updateLedAnimations();
   delay(10);                             // arbitrary delay to limit data to serial port 
 }
